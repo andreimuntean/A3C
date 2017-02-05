6e2710cd8b2a57db417a48318cc81e06456d7bac
andreimuntean <andrei.muntean.dev@gmail.com>
2017-02-05 22:21:21 +0000

    Updated shell script and started programming

---
+++ thread.py
@@ -0,0 +1,75 @@
+"""Trains an agent to play Atari games from OpenAI gym.
+
+Heavily influenced by DeepMind's seminal paper 'Asynchronous Methods for Deep Reinforcement
+Learning' (Mnih et al., 2016).
+"""
+
+import argparse
+
+
+PARSER = argparse.ArgumentParser(description='Train an agent to play Atari games.')
+
+PARSER.add_argument('--env_name',
+                    metavar='ENVIRONMENT',
+                    help='name of an OpenAI Gym Atari environment on which to train',
+                    default='Pong-v0')
+
+PARSER.add_argument('--action_space',
+                    nargs='+',
+                    help='restricts the number of possible actions',
+                    type=int)
+
+PARSER.add_argument('--load_path',
+                    metavar='PATH',
+                    help='loads a trained model from the specified path')
+
+PARSER.add_argument('--log_dir',
+                    metavar='PATH',
+                    help='directory in which summaries will be saved',
+                    default='models/tmp')
+
+PARSER.add_argument('--num_threads',
+                    metavar='THREADS',
+                    help='number of learning threads',
+                    type=int,
+                    default=multiprocessing.cpu_count())
+
+PARSER.add_argument('--train_time',
+                    metavar='TIME STEPS',
+                    help='number of time steps that each thread will train for',
+                    type=int,
+                    default=20000000)
+
+PARSER.add_argument('--learning_rate',
+                    metavar='LAMBDA',
+                    help='rate at which the network learns from new examples',
+                    type=float,
+                    default=1e-4)
+
+PARSER.add_argument('--max_gradient',
+                    metavar='DELTA',
+                    help='maximum value allowed for gradients during backpropagation',
+                    type=float,
+                    default=10)
+
+PARSER.add_argument('--discount',
+                    metavar='GAMMA',
+                    help='discount factor for future rewards',
+                    type=float,
+                    default=0.99)
+
+PARSER.add_argument('--observations_per_state',
+                    metavar='FRAMES',
+                    help='number of consecutive frames within a state',
+                    type=int,
+                    default=3)
+
+
+def main(args):
+    """Trains an agent to play Atari games."""
+
+    pass
+
+
+if __name__ == '__main__':
+    main(PARSER.parse_args())
--- train.sh
+++ train.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 ENV_NAME="Pong-v0"
-SAVE_DIR="./models/$ENV_NAME"
+LOG_DIR="./models/$ENV_NAME"
 TMUX_SESSION_NAME="a3c"
 NUM_THREADS=$(nproc --all)
+TENSORBOARD_PORT=15000
 
-# Create the save directory.
-mkdir -p $SAVE_DIR
+# Create the log directory.
+mkdir -p $LOG_DIR
 
 # Kill previous tmux session. Ignore potential "can't find session" messages.
-tmux kill-session -t $TMUX_SESSION_NAME &> /dev/null
+tmux kill-session -t $TMUX_SESSION_NAME 2> /dev/null
 
 # Initialize a new tmux session.
 tmux new-session -s $TMUX_SESSION_NAME -n master -d
@@ -24,3 +25,34 @@ tmux new-window -t $TMUX_SESSION_NAME -n tensorboard
 
 # Create a window for observing hardware usage.
 tmux new-window -t $TMUX_SESSION_NAME -n htop
+
+# Wait for tmux to finish setting up.
+sleep 1
+
+# Start the master thread, which synchronizes worker threads.
+tmux send-keys -t a3c:master "/usr/bin/python3 thread.py" \
+                             " --env_name=$ENV_NAME" \
+                             " --log_dir=$LOG_DIR" \
+                             " --num_threads=$NUM_THREADS $@" Enter
+
+# Start worker threads.
+for thread_id in $(seq 1 $NUM_THREADS); do
+    tmux send-keys -t a3c:thread-$thread_id "/usr/bin/python3 thread.py" \
+                                            " --env_name=$ENV_NAME" \
+                                            " --log_dir=$LOG_DIR" \
+                                            " --num_threads=$NUM_THREADS" \
+                                            " --worker_id=$thread_id $@" Enter
+done
+
+# Start TensorBoard.
+tmux send-keys -t a3c:tensorboard "tensorboard" \
+                                  " --port $TENSORBOARD_PORT" \
+                                  " --logdir $LOG_DIR" Enter
+
+# Start htop.
+tmux send-keys -t a3c:htop htop Enter
+
+echo "Started the learning session."
+echo "Started TensorBoard at localhost:$TENSORBOARD_PORT."
+echo "Use 'tmux attach -t $TMUX_SESSION_NAME' to connect to the session."
+echo "Use 'tmux kill-session -t $TMUX_SESSION_NAME' to end the session."
